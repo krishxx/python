2ced89c5b067c4597bbbed180f13d842ed8406d3
SriKrishna Sadula <krishna.sadula@gmail.com>
2018-05-11 07:47:04 +0530

    practice programs added

---
+++ practice/ex3.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 06:51:02 2018
+
+@author: Srikrishna.Sadula
+"""
+#Math and Numbers
+#Bodmas stands for "brackets of division, multiplication, addition, subtraction".
+
+print ("I will now count my chickens:")
+
+print ("Hens", 25 + 30 / 6)
+print ("Roosters", 100 - 25 * 3 % 4)
+
+print ("Now I will count the eggs:")
+
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print ("Is it true that 3 + 2 < 5 - 7?")
+
+print (3 + 2 < 5 - 7)
+
+print ("What is 3 + 2?", 3 + 2)
+
+print ("What is 5 - 7?", 5 - 7)
+
+print ("Oh, that's why it's False.")
+
+print ("How about some more.")
+
+print ("Is it greater?", 5 > - 2)
+print ("Is it greater or equal?", 5 >= - 2)
+print ("Is it less or equal?", 5 <= - 2)
---
+++ practice/ex4.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 07:10:58 2018
+
+@author: Srikrishna.Sadula
+"""
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+carname = 'Maruti'
+
+print ("There are", cars, "cars available.")
+print ("There are only", drivers, "drivers available.")
+print ("There will be", cars_not_driven, "empty cars today.")
+print ("We can transport", carpool_capacity, "people today.")
+print ("We have", passengers, "to carpool today.")
+print ("We need to put about", average_passengers_per_car, "in each car.")
