1b8eee40b1e01d0cd9ffe9d2db13e5be55c1a4f7
SriKrishna Sadula <krishna.sadula@gmail.com>
2018-05-25 06:37:34 +0530

    practice program 1

---
+++ practice/pr1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 25 06:14:43 2018
+
+@author: Srikrishna.Sadula
+"""
+l = []
+for i in range(2000, 3000):
+    if (i%7==0) and (i%5 != 0):
+        print (" element %r" % i)
+        l.append(i)
+        
+print (l)
