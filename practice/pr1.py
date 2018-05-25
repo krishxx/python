0e39fb39fb1e09f2576d7f2fc9c0ffad9e0523e5
SriKrishna Sadula <krishna.sadula@gmail.com>
2018-05-25 06:40:09 +0530

    practice/pr1.py modified

--- practice/pr1.py
+++ practice/pr1.py
@@ -7,7 +7,7 @@ Created on Fri May 25 06:14:43 2018
 l = []
 for i in range(2000, 3000):
     if (i%7==0) and (i%5 != 0):
-        print (" element %r" % i)
+        print (i, end=',')
         l.append(i)
         
 print (l)
